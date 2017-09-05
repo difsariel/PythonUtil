bc0a59826a60b3023e3e485cbc448e509cfebb29
lsx <liangshanxue@cloudpioneer.com.cn>
2017-09-05 10:01:37 +0800

    将dict list转化为DataFrame

--- xml2csv.py
+++ xml2csv.py
@@ -1,6 +1,8 @@
 from pandas import json
 
 import xmltodict
+import pandas as pd
+
 
 def transform_to_xmllist(lines, root_element):
     combine = False
@@ -18,17 +20,18 @@ def transform_to_xmllist(lines, root_element):
     return xml_str_list
 
 
-def parse_to_dict(d):
+def parse_to_standard_dict(d):
     measurements = json.dumps(d, ensure_ascii=False)
     data_dict = json.loads(measurements)
     return data_dict
 
 
 if __name__ == "__main__":
-    path = "/Users/lsx/Downloads/搜狗实验室/SogouCS.WWW08.txt"
-    with open(path, "r") as f:
+    path = "D:/搜狗实验室/SogouCS.WWW08.txt"
+    with open(path, "r", encoding="utf-8") as f:
         lines = f.readlines()
         xml_str_list = transform_to_xmllist(lines, "doc")
         xml_dict_list = list(map(xmltodict.parse, xml_str_list))
-        xml_dict_list = list(map(parse_to_dict, xml_dict_list))
-        print(xml_dict_list)
+        xml_dict_list = list(map(parse_to_standard_dict, xml_dict_list))
+        xml_df = pd.DataFrame(xml_dict_list)
+        print(xml_df)
