08021659f150988b3c74611487f07fc03eb48f43
lsx <liangshanxue@cloudpioneer.com.cn>
2017-09-06 15:44:58 +0800

    调用自定义jar包必须加入环境变量ClassPath中，且路径中不能含中文

--- pyjnius_test.py
+++ pyjnius_test.py
@@ -1,7 +1,11 @@
-from jnius import autoclass
+import jnius_config
 
+jnius_config.add_classpath(u"D:\pyjnius\MyAwesomeSpark.jar")
+
+import jnius
 
 if __name__ == "__main__":
-    autoclass('java.lang.System').out.println('Hello world')
-    Test = autoclass('Test')
+    class_path = jnius_config.get_classpath()
+    print(class_path)
+    Test = jnius.autoclass('Test')
     Test.test()
